c5f9d8f8095a91a8b22f7dd7e81449180586b1ac
[dyad] <kanekigalvez@gmail.com>
2025-11-10 16:42:23 -0500

    [dyad] Replacing header video - wrote 1 file(s)

--- src/components/VideoHeader.tsx
+++ src/components/VideoHeader.tsx
@@ -1,9 +1,9 @@
 export const VideoHeader = () => {
   return (
     <section className="w-full">
-      <div style={{ padding: '30% 0 0 0', position: 'relative' }}>
+      <div style={{ padding: '75% 0 0 0', position: 'relative' }}>
         <iframe
-          src="https://player.vimeo.com/video/1135508272?background=1&autoplay=1&loop=1&muted=1"
+          src="https://player.vimeo.com/video/1135515066?background=1&autoplay=1&loop=1&muted=1"
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
           style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
