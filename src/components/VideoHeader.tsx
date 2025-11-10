f82e436652ba94367f9247d1666b17f21e9a0ab7
[dyad] <kanekigalvez@gmail.com>
2025-11-10 16:29:41 -0500

    [dyad] Hiding Vimeo player controls - wrote 1 file(s)

--- src/components/VideoHeader.tsx
+++ src/components/VideoHeader.tsx
@@ -3,7 +3,7 @@ export const VideoHeader = () => {
     <section className="w-full">
       <div style={{ padding: '75% 0 0 0', position: 'relative' }}>
         <iframe
-          src="https://player.vimeo.com/video/1135508272?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&muted=1&loop=1"
+          src="https://player.vimeo.com/video/1135508272?background=1&autoplay=1&loop=1&muted=1"
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
           style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
