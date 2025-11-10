230b5ed505c1e1e3aca6a82592ef0f4fde243aeb
[dyad] <kanekigalvez@gmail.com>
2025-11-10 16:37:22 -0500

    [dyad] Restoring deleted video header - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,11 +5,13 @@ import { DtcSearchSection } from "@/components/DtcSearchSection";
 import { ProductsSection } from "@/components/ProductsSection";
 import { FeaturesSection } from "@/components/FeaturesSection";
 import { Footer } from "@/components/Footer";
+import { VideoHeader } from "@/components/VideoHeader";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <VideoHeader />
       <main>
         <HeroSection />
         <CompatibilitySection />
