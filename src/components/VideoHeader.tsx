d7997a85d0dd12fefe62f93dc2affe932caaee80
[dyad] <kanekigalvez@gmail.com>
2025-11-10 16:49:39 -0500

    [dyad] Resizing and adjusting header video - wrote 1 file(s)

--- src/components/VideoHeader.tsx
+++ src/components/VideoHeader.tsx
@@ -1,24 +1,26 @@
 export const VideoHeader = () => {
   return (
-    <section className="w-full py-12">
+    <section className="w-full py-4">
       <div className="container">
-        <div
-          className="rounded-lg overflow-hidden shadow-xl"
-          style={{ padding: "75% 0 0 0", position: "relative" }}
-        >
-          <iframe
-            src="https://player.vimeo.com/video/1135516907?background=1&autoplay=1&loop=1&muted=1"
-            frameBorder="0"
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-            }}
-            title="Header Video"
-          ></iframe>
+        <div className="max-w-5xl mx-auto">
+          <div
+            className="overflow-hidden"
+            style={{ padding: "56.25% 0 0 0", position: "relative" }}
+          >
+            <iframe
+              src="https://player.vimeo.com/video/1135516907?background=1&autoplay=1&loop=1&muted=1"
+              frameBorder="0"
+              allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+              }}
+              title="Header Video"
+            ></iframe>
+          </div>
         </div>
       </div>
     </section>
