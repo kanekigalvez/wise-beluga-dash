a1d61ad04ce0f52334b4c7ec311e09d612cf9802
[dyad] <kanekigalvez@gmail.com>
2025-11-12 16:29:24 -0500

    [dyad] Añadiendo un chatbot de IA - wrote 5 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,6 +19,7 @@ import {
 import { slugify } from "@/lib/utils";
 import { useTranslation } from "react-i18next";
 import { LanguageSwitcher } from "./LanguageSwitcher";
+import { ChatBot } from "./ChatBot";
 
 const allProducts = [
     { name: "Golo ED+" }, { name: "iDiag for Android" }, { name: "TD1" },
@@ -101,6 +102,7 @@ export const Header = () => {
                 <span className="text-xs">⌘</span>K
               </kbd>
             </Button>
+            <ChatBot />
             <LanguageSwitcher />
             <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
               <SheetTrigger asChild>
